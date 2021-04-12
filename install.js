88f7914bca6508120b1f1c954ba1975fa7eac33b
Hunter Shaw <hunter.shaw@classyllama.com>
2021-04-12 08:49:32 -0500

    Add graceful install failure

--- install.js
+++ install.js
@@ -1,22 +1,29 @@
 const fs = require('fs')
 
-const base = fs.readFileSync('../../templates/layout/base.html', 'utf8')
-const empty = fs.readFileSync('../../templates/layout/empty.html', 'utf8')
+try {
+    const base = fs.existsSync('../../templates/layout/base.html') ? fs.readFileSync('../../templates/layout/base.html', 'utf8') : undefined
+    const empty = fs.existsSync('../../templates/layout/empty.html') ? fs.readFileSync('../../templates/layout/empty.html', 'utf8') : undefined
 
-const installation = '<script id="BC_GraphQL_Token" type="application/json">"{{settings.storefront_api.token}}"</script>'
+    const installation = '<script id="BC_GraphQL_Token" type="application/json">"{{settings.storefront_api.token}}"</script>'
 
-function install(contents){
-    if (contents.includes(installation)) return contents
+    function install(contents){
+        if (contents.includes(installation)) return contents
 
-    const splitter = contents.match(/<body.*>/)
+        const splitter = contents.match(/<body.*>/)
 
-    const parts = contents.split(splitter)
+        const parts = contents.split(splitter)
 
-    return `${parts[0]}
-    ${splitter}
-        ${installation}
-    ${parts[1]}`
+        return `${parts[0]}
+        ${splitter}
+            ${installation}
+        ${parts[1]}`
+    }
+
+    if (base !== undefined) fs.writeFileSync('../../templates/layout/base.html', install(base))
+    if (empty !== undefined) fs.writeFileSync('../../templates/layout/empty.html', install(empty))
+} catch (e){
+    console.error(e.message)
+    console.error('bigcommerce-graphql install failed. Check output above. Please manually install the following token into layout files:')
+    console.log('<script id="BC_GraphQL_Token" type="application/json">"{{settings.storefront_api.token}}"</script>')
 }
 
-fs.writeFileSync('../../templates/layout/base.html', install(base))
-fs.writeFileSync('../../templates/layout/empty.html', install(empty))
