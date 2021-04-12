afc1d0e824c01ceb1958742d7e24646e23a2637b
Hunter Shaw <hunter.shaw@classyllama.com>
2021-04-12 13:52:16 -0500

    Add generic installer

--- install.js
+++ install.js
@@ -1,29 +1,36 @@
 const fs = require('fs')
 
-try {
-    const base = fs.existsSync('../../templates/layout/base.html') ? fs.readFileSync('../../templates/layout/base.html', 'utf8') : undefined
-    const empty = fs.existsSync('../../templates/layout/empty.html') ? fs.readFileSync('../../templates/layout/empty.html', 'utf8') : undefined
+const installation = '<script id="BC_GraphQL_Token" type="application/json">"{{settings.storefront_api.token}}"</script>'
 
-    const installation = '<script id="BC_GraphQL_Token" type="application/json">"{{settings.storefront_api.token}}"</script>'
-
-    function install(contents){
-        if (contents.includes(installation)) return contents
-
-        const splitter = contents.match(/<body.*>/)
-
-        const parts = contents.split(splitter)
-
-        return `${parts[0]}
-        ${splitter}
-            ${installation}
-        ${parts[1]}`
+function install(contents){
+    if (contents.includes(installation)) {
+        console.log('Already installed.')
+        return contents
     }
 
-    if (base !== undefined) fs.writeFileSync('../../templates/layout/base.html', install(base))
-    if (empty !== undefined) fs.writeFileSync('../../templates/layout/empty.html', install(empty))
-} catch (e){
-    console.error(e.message)
-    console.error('bigcommerce-graphql install failed. Check output above. Please manually install the following token into layout files:')
-    console.log('<script id="BC_GraphQL_Token" type="application/json">"{{settings.storefront_api.token}}"</script>')
+    const splitter = contents.match(/<body.*>/)
+
+    if (splitter === null) {
+        console.log('No body tag found. Not installing.')
+        return contents
+    }
+
+    const parts = contents.split(splitter)
+
+    console.log('Installing.')
+    return `${parts[0]}
+${splitter}${installation}
+${parts[1] !== undefined ? parts[1] : ''}`
 }
 
+try {
+    const files = fs.readdirSync('../../templates/layout/')
+    for (const file of files) {
+        console.log(`Checking installation in templates/layout/${file}:`)
+        fs.writeFileSync(`../../templates/layout/${file}`, install(fs.readFileSync(`../../templates/layout/${file}`, 'utf-8')))
+    }
+} catch (e) {
+    console.error(e.message)
+    console.error('bigcommerce-graphql install failed. Check output above. Please manually install the following token into all layout files after the <body> tag:')
+    console.log(installation)
+}
