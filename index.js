f35ca7488f7fb0b2f67723d0985354a29732058e
Matt Campbell <matt.campbell@classyllama.com>
2021-06-29 13:03:14 -0500

    Fix nested clean issue
    - Minor clean up per ESLint warnings
    - Fixed an issue where the clean function would not work for nested arrays.

--- index.js
+++ index.js
@@ -5,29 +5,45 @@ const token = JSON.parse(document.getElementById('BC_GraphQL_Token').textContent
  * @async
  * @param {string} query GraphQL query without 'query' keyword. Example: `{ site { categoryTree { name } } }`
  */
-async function get(query, removeEdges=true){
+async function get(query, removeEdges=true) {
     const response = await fetch('/graphql', {
         method: 'post',
         headers: {
-         'Content-Type': 'application/json',
-         'Authorization': `Bearer ${token}`
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
         },
         body: JSON.stringify({query})
     });
-    if (response.ok)
-        return removeEdges ? clean((await response.json()).data) : (await response.json()).data
-    else
+
+    if (response.ok) {
+        const { data } = await response.json()
+
+        return removeEdges
+            ? clean(data)
+            : data
+    } else
         throw Error(`GraphQL error ${response.status} - ${response.statusText}`)
 }
 
 function clean(data){
     for (let property in data){
-        if (data[property].edges !== undefined) {
-            data[property]
-            data[property] = data[property].edges.map((element)=>element.node)
+        const hasEdge = !!data[property].edges
+
+        if (hasEdge)
+            data[property] = data[property].edges.map((element) => element.node)
+
+        const isObject = typeof data[property] === 'object'
+        const isArray = Array.isArray(data[property])
+
+        if (isObject && !isArray)
+            clean(data[property])
+
+        if (isArray) {
+            for (const i of data[property])
+                clean(i)
         }
-        else if (typeof data[property] === 'object' && !Array.isArray(data[property])) clean(data[property])
     }
+
     return data
 }
 
